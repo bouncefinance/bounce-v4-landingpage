955ca9144b13cadb958fd5340fef837cd62a7216
dev <dev@bounce.finance>
2024-05-18 00:52:05 +0900

    fix booster style

--- src/components/boost/TopBlock/index.tsx
+++ src/components/boost/TopBlock/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTopBlockStyles } from './useTopBlockStyles';
-import { Container, Typography } from '@material-ui/core';
+import { Box, Container, Typography } from '@material-ui/core';
 import { WithAnimation } from 'src/modules/WithAnimation';
 import ComBtn from 'src/components/common/ComBtn';
 import { useIsMDDown } from '../../../modules/theme';
@@ -11,38 +11,43 @@ export const TopBlock = () => {
   return (
     <div className={classes.pageRoot}>
       <Container maxWidth={false} className={classes.pageContainer}>
-        <WithAnimation
-          className={classes.title1}
-          Component={Typography}
-          variant="h2"
-        >
-          Bounce Booster
-        </WithAnimation>
-        <WithAnimation
-          className={classes.title2}
-          Component={Typography}
-          variant="h2"
-        >
-          Maximize Yield,
-        </WithAnimation>
-        <WithAnimation
-          className={classes.title2}
-          Component={Typography}
-          variant="h2"
-        >
-          Minimize Effort.
-        </WithAnimation>
+        <Box>
+          <WithAnimation
+            className={classes.title1}
+            Component={Typography}
+            variant="h2"
+          >
+            Bounce Booster
+          </WithAnimation>
+          <WithAnimation
+            className={classes.title2}
+            Component={Typography}
+            variant="h2"
+          >
+            Maximize Yield,
+          </WithAnimation>
+          <WithAnimation
+            className={classes.title2}
+            Component={Typography}
+            variant="h2"
+          >
+            Minimize Effort.
+          </WithAnimation>
+          <WithAnimation className={classes.btnBox}>
+            <ComBtn
+              disabled
+              text={'Coming Soon'}
+              handleClick={() => {
+                window.open(
+                  'https://app.bounce.finance/TokenAuction',
+                  '_blank',
+                );
+              }}
+              style={{ background: '#2B51DA', pointerEvents: 'none' }}
+            ></ComBtn>
+          </WithAnimation>
+        </Box>
 
-        <WithAnimation className={classes.btnBox}>
-          <ComBtn
-            disabled
-            text={'Coming Soon'}
-            handleClick={() => {
-              window.open('https://app.bounce.finance/TokenAuction', '_blank');
-            }}
-            style={{ background: '#2B51DA', pointerEvents: 'none' }}
-          ></ComBtn>
-        </WithAnimation>
         <WithAnimation
           defaultAnimation={false}
           rootMargin={'50%'}
